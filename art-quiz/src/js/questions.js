0733cc948c173ba24634a9c6077076ed3763c425
Denis Gusar <degusar.wot@gmail.com>
2021-11-10 22:10:42 +0300

    feat: add questions to picture part

--- art-quiz/src/js/questions.js
+++ art-quiz/src/js/questions.js
@@ -14,11 +14,19 @@ function getRandom(min, max, dontRepeat) {
    }
 }
 
+function getRandomPicture(min, max, dontRepeat) {
+    let result = Math.round(Math.random() * (max - min) + min);
+    if (dontRepeat.includes(result)) {
+        return getRandomPicture(min, max, dontRepeat)
+     } else {
+        return result
+    }
+}
+
 class Question {
-  
     constructor(index) {
-        this.image =images[index].imageNum;
-        this.rightAnswer = images[this.image].author;
+        this.question =images[index].imageNum;
+        this.rightAnswer = images[this.question].author;
         this.firstWrong = getRandom(0, authorSet.length - 1,[this.rightAnswer]);
         this.secondWrong = getRandom(0, authorSet.length - 1, [this.rightAnswer, this.firstWrong]);
         this.thirdWrong = getRandom(0, authorSet.length - 1,[this.rightAnswer, this.firstWrong, this.secondWrong]);
@@ -26,9 +34,24 @@ class Question {
         
 }
 
+class QuestionPictures {
+    constructor(index) {
+        this.question = images[index].author;
+        this.rightAnswer = +images[index].imageNum;
+        this.firstWrong = getRandomPicture(0, images.length - 1, [+this.rightAnswer])
+        this.secondWrong = getRandomPicture(0, images.length - 1, [+this.rightAnswer, +this.firstWrong]);
+        this.thirdWrong = getRandomPicture(0, images.length - 1,[+this.rightAnswer, +this.firstWrong, +this.secondWrong]);
+    }
+    
+}
+
 let questions = [];
 for(let i=0; i<120;i++) {
    questions.push(new Question(i)) 
+};
+
+for (let i = 120; i < images.length; i++) {
+    questions.push(new QuestionPictures(i))
 };
 
 class Category {
@@ -58,4 +81,10 @@ let category = [];
 for (let i = 0; i < 12; i++) {
     category.push(new Category(i, questions));
 }
+for (let i = 12; i < 24; i++) {
+    category.push(new Category(i, questions))
+}
 
+console.group('Category');
+console.log(category);
+console.groupEnd();

6ed5bc495cf2ed62d8e7b2e8b03eb88d96d70243
Denis Gusar <degusar.wot@gmail.com>
2021-11-23 00:27:51 +0300

    fix: delete repeated answers

--- art-quiz/src/js/questionsPictures.js
+++ art-quiz/src/js/questionsPictures.js
@@ -1,7 +1,27 @@
 import { images } from "../imagesEn";
 import { shuffle } from "./questionsAuthors";
 
+let allAnswers = [];
+
+    images.forEach((element) => {
+      allAnswers.push([element.author, element.imageNum]);
+    });
+
+let possibleAnswers = [];
+function allRightAnswers(author) {
+    possibleAnswers = []
+    for (let i = 0; i < allAnswers.length; i++) {
+        if (allAnswers[i][0] === author) {
+          possibleAnswers.push(+allAnswers[i][1]);
+        }
+    }
+   return possibleAnswers
+}
+
+
+
 function getRandomPicture(min, max, dontRepeat) {
+
     let result = Math.round(Math.random() * (max - min) + min);
     if (dontRepeat.includes(result)) {
         return getRandomPicture(min, max, dontRepeat)
@@ -14,9 +34,9 @@ export class QuestionPictures {
         this.question = `What artwork was painted by ${images[index].author}?`;
         this.questionIndex = index;
         this.rightAnswer = +images[index].imageNum;
-        this.firstWrong = getRandomPicture(0, images.length - 1, [this.rightAnswer])
-        this.secondWrong = getRandomPicture(0, images.length - 1, [this.rightAnswer, this.firstWrong]);
-        this.thirdWrong = getRandomPicture(0, images.length - 1, [this.rightAnswer, this.firstWrong, this.secondWrong]);
+        this.firstWrong = getRandomPicture(0, images.length - 1, [this.rightAnswer].concat(allRightAnswers(images[index].author)))
+        this.secondWrong = getRandomPicture(0, images.length - 1, [this.rightAnswer, this.firstWrong].concat(allRightAnswers(images[index].author)));
+        this.thirdWrong = getRandomPicture(0, images.length - 1, [this.rightAnswer, this.firstWrong, this.secondWrong].concat(allRightAnswers(images[index].author)));
         this.answers = shuffle([this.rightAnswer, this.firstWrong, this.secondWrong, this.thirdWrong]);
         this.year = images[index].year;
         this.name = images[index].name;
